5f5490679140902f166fbaccc3befdfff445e915
Kevin Nguyen <ngynkvn@gmail.com>
2024-08-09 15:51:05 -0400

    fix: Add async to conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,6 +1,10 @@
 return {
   "stevearc/conform.nvim",
   opts = {
+    default_format_opts = {
+      async = true,
+      lsp_format = "fallback", -- not recommended to change
+    },
     formatters_by_ft = {
       javascript = { "prettier" },
       javascriptreact = { "prettier" },
