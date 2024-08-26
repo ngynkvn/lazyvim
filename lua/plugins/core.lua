746992ae9958dcfb4167314f488d41c1c370acc2
Kevin <ngynkvn@gmail.com>
2024-08-26 09:11:19 -0400

    chore: sync

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,8 +1,31 @@
 return {
   {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "luvit-meta/library", words = { "vim%.uv" } },
+      },
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin",
     },
   },
+  -- Lazy
+  -- {
+  --   "crnvl96/lazydocker.nvim",
+  --   event = "VeryLazy",
+  --   keys = {
+  --     { "\\\\", ":LazyDocker<CR>", desc = "Open lazydocker" },
+  --   },
+  --   opts = {}, -- automatically calls `require("lazydocker").setup()`
+  --   dependencies = {
+  --     "MunifTanjim/nui.nvim",
+  --   },
+  -- },
 }
