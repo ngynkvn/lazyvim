10cf265d03edac05f184a47b412dba8e81c73037
Kevin Nguyen <ngynkvn@gmail.com>
2024-08-27 20:39:28 -0400

    update: Add prettier LazyExtra

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -5,11 +5,5 @@ return {
       async = true,
       lsp_format = "fallback", -- not recommended to change
     },
-    formatters_by_ft = {
-      javascript = { "prettier" },
-      javascriptreact = { "prettier" },
-      typescript = { "prettier" },
-      typescriptreact = { "prettier" },
-    },
   },
 }
