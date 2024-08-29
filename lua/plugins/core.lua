f2c74d4dfa9995fef2b16dc19eacb9d2ef2e3313
Kevin Nguyen <ngynkvn@gmail.com>
2024-08-29 12:47:12 -0400

    feat: Add arrow

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -27,6 +27,14 @@ return {
       })
     end,
   },
+  {
+    "otavioschwanck/arrow.nvim",
+    opts = {
+      show_icons = true,
+      leader_key = ";", -- Recommended to be a single key
+      buffer_leader_key = "m", -- Per Buffer Mappings
+    },
+  },
   -- Lazy
   -- {
   --   "crnvl96/lazydocker.nvim",
