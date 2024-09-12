93458092fe89cda3c8766b22f4080a1e3585c4b1
Kevin <ngynkvn@gmail.com>
2024-09-11 21:30:10 -0400

    feat: Add zig config

---
+++ lua/plugins/zig.lua
@@ -0,0 +1,15 @@
+-- Zig specific setup
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "zig" } },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        zls = {},
+      },
+    },
+  },
+}
