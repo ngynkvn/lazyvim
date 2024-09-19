835d52f8c5401b8cbde9d4d4b6b7e65b8be231d8
Kevin Nguyen <ngynkvn@gmail.com>
2024-09-19 11:56:39 -0400

    chore: nnoremap 'ga' to nop, add mini.visits and mini.align

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -12,27 +12,28 @@ return {
       },
     },
   },
-  {
-    "otavioschwanck/arrow.nvim",
-    opts = {
-      show_icons = true,
-      leader_key = "<leader>h", -- Recommended to be a single key
-      buffer_leader_key = "m", -- Per Buffer Mappings
-    },
-  },
-  -- God that was so fucking hard to figure out
-  {
-    "folke/which-key.nvim",
-    ---@module 'which-key'
-    ---@param opts wk.Opts
-    opts = function(_, opts)
-      table.insert(opts.spec, {
-        "<leader>h",
-        icon = { cat = "filetype", name = "harpoon" },
-        desc = "Arrow",
-      })
-    end,
-  },
+  -- NOTE: Evaluating echasnovski/mini.visits
+  -- {
+  --   "otavioschwanck/arrow.nvim",
+  --   opts = {
+  --     show_icons = true,
+  --     leader_key = "<leader>h", -- Recommended to be a single key
+  --     buffer_leader_key = "m", -- Per Buffer Mappings
+  --   },
+  -- },
+  -- -- God that was so fucking hard to figure out
+  -- {
+  --   "folke/which-key.nvim",
+  --   ---@module 'which-key'
+  --   ---@param opts wk.Opts
+  --   opts = function(_, opts)
+  --     table.insert(opts.spec, {
+  --       "<leader>h",
+  --       icon = { cat = "filetype", name = "harpoon" },
+  --       desc = "Arrow",
+  --     })
+  --   end,
+  -- },
   -- Lazy
   -- {
   --   "crnvl96/lazydocker.nvim",
