fbe67a1c005997dca6a213a3c0e3fd7fc71a5bfd
Kevin Nguyen <ngynkvn@gmail.com>
2024-09-20 14:17:22 -0400

    chore: Remove extra dependency

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,7 +2,6 @@ return {
   {
     "akinsho/bufferline.nvim",
     version = "*",
-    dependencies = "nvim-tree/nvim-web-devicons",
     opts = {
       options = {
         always_show_bufferline = true,
