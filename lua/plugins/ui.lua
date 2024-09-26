caf8c0330bcfe983ea4feb79c586855bb0ae9375
Kevin <ngynkvn@gmail.com>
2024-09-26 19:14:50 -0400

    chore: update plugins

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,32 +1,20 @@
 return {
-  -- ui.bufferline.lua --
   {
     "akinsho/bufferline.nvim",
     version = "*",
     dependencies = "nvim-tree/nvim-web-devicons",
-    opts = {
-      options = { always_show_bufferline = true },
-    },
+    opts = { options = { always_show_bufferline = true } },
   },
-  {
-    "tiagovla/scope.nvim",
-    dependencies = "akinsho/bufferline.nvim",
-    opts = {},
-  },
-  --   {
-  --     "lualine.nvim",
-  --     opts = {},
-  --   },
-  -- ui.neotree.lua --
+  { "folke/trouble.nvim", opts = { max_items = false } },
+  { "tiagovla/scope.nvim", dependencies = "akinsho/bufferline.nvim", opts = {} },
+  --{ "lualine.nvim", opts = {}, },
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
       window = {
         position = "right",
-        width = 18,
+        width = 20,
       },
     },
   },
-  -- ui.trouble.lua
-  { "folke/trouble.nvim", opts = { max_items = false } },
 }
