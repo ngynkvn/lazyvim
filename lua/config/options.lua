84f32ce6436a70d3aa921082d65a94bc31ddba7a
Kevin <ngynkvn@gmail.com>
2024-10-27 21:50:51 -0400

    fix: dumb markdown thing
    https://github.com/nvim-treesitter/nvim-treesitter/issues/5751#issuecomment-2311310008

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -14,3 +14,15 @@ vim.filetype.add({
     [".*%.[eg]?bnf"] = "ebnf",
   },
 })
+
+-- https://github.com/nvim-treesitter/nvim-treesitter/issues/5751#issuecomment-2311310008
+require("vim.treesitter.query").set(
+  "markdown",
+  "highlights",
+  [[
+;From MDeiml/tree-sitter-markdown
+[
+  (fenced_code_block_delimiter)
+] @punctuation.delimiter
+]]
+)
