64e1a7726bb9e1c3ff1b7f1d8cdbf3eaf9caff57
Kevin <ngynkvn@gmail.com>
2025-01-05 22:22:10 -0500

    fix: add borders to completion
    A  lua/plugins/cmp.lua

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      completion = {
+        menu = { border = "single" },
+        documentation = { window = { border = "single" } },
+      },
+      signature = { window = { border = "single" } },
+    },
+  },
+}
