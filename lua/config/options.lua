d228b1b8fe3a82d6e3be0d3a994a93820d4785e5
Kevin <ngynkvn@gmail.com>
2025-01-17 17:46:20 -0500

    M  lua/config/options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,7 +7,7 @@ vim.opt.relativenumber = false
 -- popup max height
 vim.opt.pumheight = 0
 -- Preferred textwidth
-vim.opt.textwidth = 120
+vim.opt.textwidth = 80
 
 --vim.opt.winblend = 10
 --vim.opt.pumblend = 10
