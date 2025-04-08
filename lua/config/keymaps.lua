d3a12c7a735c343c14962f59749c027b0e406129
Kevin <ngynkvn@gmail.com>
2025-04-08 01:22:24 -0400

    A  colors/tokyonight-night-blu.lua M  lazy-lock.json M  lua/config/keymaps.lua M  lua/config/options.lua M  stylua.toml

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -15,35 +15,8 @@ vim.api.nvim_create_user_command("CopyPath", function()
   vim.notify('Copied "' .. path .. '" to the clipboard!')
 end, { desc = "Copy buffer path to clipboard" })
 
-vim.api.nvim_create_user_command("LspAction", function(opts)
-  vim.lsp.buf.code_action({
-    context = {
-      diagnostics = {},
-      only = { opts.args },
-    },
-  })
-end, {
-  nargs = "?",
-  complete = function()
-    -- Common code action kinds
-    return {
-      "quickfix",
-      "refactor",
-      "refactor.extract",
-      "refactor.inline",
-      "refactor.rewrite",
-      "source",
-      "source.organizeImports",
-      "source.fixAll",
-    }
-  end,
-})
 vim.keymap.set("ca", "rl", "RustLsp")
 
-vim.keymap.set("i", "<C-h>", "<Left>")
-vim.keymap.set("i", "<C-j>", "<Down>")
-vim.keymap.set("i", "<C-k>", "<Up>")
-vim.keymap.set("i", "<C-l>", "<Right>")
 vim.keymap.set("i", "<C-h>", "<Left>")
 vim.keymap.set("i", "<C-j>", "<Down>")
 vim.keymap.set("i", "<C-k>", "<Up>")
