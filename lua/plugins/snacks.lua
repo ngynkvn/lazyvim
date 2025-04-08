88b46b4a9bbcf1bdc96b98091333144e22e93694
Kevin Nguyen <ngynkvn@gmail.com>
2025-04-08 13:36:31 -0400

    right side is superior

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,10 +1,12 @@
----@module 'snacks'
-
 return {
   "folke/snacks.nvim",
   ---@type snacks.Config
   opts = {
     scroll = { enabled = false },
-    explorer = {},
+    -- see ui.lua for layout config
+    explorer = {
+      replace_netrw = true,
+    },
+    picker = { sources = { explorer = { layout = { layout = { position = "right" } } } } },
   },
 }
